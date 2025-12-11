9f1bddc912953a3b48ca0f010c90008eea96cb76
Nauf <130215373+naufudge@users.noreply.github.com>
2025-12-11 19:53:44 +0500

    nativewind init

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/_layout.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
